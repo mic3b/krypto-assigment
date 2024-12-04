a42b26e8cef9a39d84f83f47bb98044f7657316d
mikolajbubacz <mikolaj.bubacz@gmail.com>
2024-12-04 20:32:06 +0100

    change sitemaps

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.PUBLIC_URL || "kryptonum-faq.vercel.app";
+  const baseUrl = process.env.PUBLIC_URL || "krypto-assigment.vercel.app";
 
   return [
     {
