2b69c54732895b176fcad7b8b6d26ffa7064fa47
mikolajbubacz <mikolaj.bubacz@gmail.com>
2024-12-04 20:46:26 +0100

    again chagnes in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.URL || "kryptoneum-faq.vercel.app";
+  const baseUrl = process.env.URL || "https://krypto-assigment.vercel.app/";
   return {
     rules: {
       userAgent: "*",
